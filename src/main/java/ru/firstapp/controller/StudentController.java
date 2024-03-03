3ea556a80a23d9cc57726573884adcfedbcaf875
chebotop <chebotop@gmail.com>
2024-03-04 00:36:07 +0300

    continue to controllers fix tomorrow

--- src/main/java/ru/firstapp/controller/StudentController.java
+++ src/main/java/ru/firstapp/controller/StudentController.java
@@ -2,9 +2,11 @@ package ru.firstapp.controller;
 
 import lombok.AllArgsConstructor;
 import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.firstapp.dto.StudentDTO;
+import ru.firstapp.dto.StudentUpdateDTO;
 import ru.firstapp.model.Student;
 import ru.firstapp.service.StudentService;
 
@@ -35,14 +37,21 @@ public class StudentController {
         return studentService.findByEmail(email);
     }
 
-    // @RequestBody реализует JSON
-    @PutMapping("update_student")
-    public Student updateStudent(@RequestBody Student student) {
-        return studentService.updateStudent(student);
+    //todo реализуем обновление данных студента
+    @PutMapping("/update_student/{email}")
+    public ResponseEntity<?> updateStudent(@PathVariable String email, @RequestBody StudentUpdateDTO studentUpdateDTO) {
+        Student student = studentService.findByEmail(email);
+        if (student == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found");
+        }
+        student.setFirstName(studentUpdateDTO.getFirstName());
+        student.setLastName(studentUpdateDTO.getLastName());
+        student.setEmail(studentUpdateDTO.getEmail());
     }
-
-    @DeleteMapping("delete_student/{email}")
-    public void deleteStudent(@PathVariable String email) {
-        studentService.deleteStudent(email);
+        @DeleteMapping("delete_student/{email}")
+        public void deleteStudent (@PathVariable String email){
+            studentService.deleteStudent(email);
+        }
     }
 }
+
--- src/main/java/ru/firstapp/controller/TeacherController.java
+++ src/main/java/ru/firstapp/controller/TeacherController.java
@@ -1,10 +1,12 @@
 package ru.firstapp.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.firstapp.dto.TeacherDTO;
+import ru.firstapp.mappers.TeacherMapper;
 import ru.firstapp.model.Course;
 import ru.firstapp.model.Teacher;
 import ru.firstapp.service.CourseService;
@@ -17,7 +19,7 @@ import java.util.List;
 public class TeacherController {
     private final TeacherService service;
     private final CourseService courseService;
-
+    private final TeacherMapper teacherMapper;
     @GetMapping
     public List<Teacher> findAllTeacher() {
         return service.findAllTeacher();
@@ -27,13 +29,14 @@ public class TeacherController {
     @PostMapping("save_teacher")// сделать метод POST
     public ResponseEntity<TeacherDTO> saveTeacher(@RequestBody TeacherDTO teacherDTO) {
         // Шаг 1: Преобразование из TeacherDTO в сущность Teacher
-        Teacher teacher = convertToEntity(teacherDTO);
-
+//        Teacher teacher = convertToEntity(teacherDTO);
+        Teacher teacher = teacherMapper.toTeacher(teacherDTO);
         // Шаг 2: Сохранение сущности Teacher в базу данных
         Teacher savedTeacher = service.saveTeacher(teacher);
-
         // Шаг 3: Преобразование сохраненного учителя обратно в TeacherDTO
-        TeacherDTO savedTeacherDTO = convertToDTO(savedTeacher);
+//        TeacherDTO savedTeacherDTO = convertToDTO(savedTeacher);
+        TeacherDTO savedTeacherDTO = teacherMapper.toTeacherDTO(savedTeacher);
+
 
         // Если есть courseIds, установите связь с курсами
         if (teacherDTO.getId() != null && !teacherDTO.getListCourseIds().isEmpty()) {
@@ -45,50 +48,21 @@ public class TeacherController {
         return new ResponseEntity<>(savedTeacherDTO, HttpStatus.CREATED);
     }
 
-    // Пример метода для преобразования DTO в сущность
-    private Teacher convertToEntity(TeacherDTO teacherDTO) {
-        // Здесь ваш код для преобразования
-        // Можно использовать ModelMapper или вручную преобразовать поля DTO в поля сущности
-        Teacher teacher = new Teacher();
-        teacher.setFirstName(teacherDTO.getFirstName());
-        teacher.setLastName(teacherDTO.getLastName());
-        teacher.setEmail(teacherDTO.getEmail());
-        teacher.setDateOfBirth(teacherDTO.getDateOfBirth());
-        // Дополните другие поля по необходимости
-        return teacher;
-    }
-
-    // Пример метода для преобразования сущности в DTO
-    private TeacherDTO convertToDTO(Teacher savedTeacher) {
-        // Здесь ваш код для преобразования
-        TeacherDTO teacherDTO = new TeacherDTO();
-        teacherDTO.setId(savedTeacher.getId());
-        teacherDTO.setFirstName(savedTeacher.getFirstName());
-        teacherDTO.setLastName(savedTeacher.getLastName());
-        teacherDTO.setEmail(savedTeacher.getEmail());
-        teacherDTO.setDateOfBirth(savedTeacher.getDateOfBirth());
-        // Дополните другие поля по необходимости
-        return teacherDTO;
-    }
 
     @GetMapping("/{email}")    // используем URL адрес для получения почты
     // используем аннотацию для извлечения переменной из URL-запроса и передачи в метод контроллера как параметра
     public Teacher findByEmail(@PathVariable String email) { // имя получаемой переменной и название в фигурных скобках совпадают
         return service.findByEmail(email);
     }
-
     @GetMapping("/{id}")
     public Teacher findById(@PathVariable Long id) { // имя получаемой переменной и название в фигурных скобках совпадают
         return service.findById(id);
     }
-
-
     // @RequestBody реализует JSON
     @PutMapping("update_teacher")
     public Teacher updateTeacher(@RequestBody Teacher teacher) {
         return service.updateTeacher(teacher);
     }
-
     @DeleteMapping("delete_teacher/{email}")
     public void deleteTeacher(@PathVariable String email) {
         service.deleteTeacher(email);
